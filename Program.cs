387f4bff841ab2c58ea2e5720f55c083c8495186
Fleck Johannes <Johannes.Fleck@k-business.com>
2024-01-24 22:05:54 +0100

    sd

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using navapi_scaffolding.Models;
 
 var optBuilder = new DbContextOptionsBuilder<DxpNavAtDataContainerContext>();
-optBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=DxpNavAtData_container;Integrated Security=SSPI;TrustServerCertificate=true;");
+optBuilder.UseSqlServer(
+    "Data Source=.\\SQLEXPRESS;Initial Catalog=DxpNavAtData_container;Integrated Security=SSPI;TrustServerCertificate=true;");
 var ctx = new DxpNavAtDataContainerContext(optBuilder.Options);
 
 
@@ -12,27 +13,66 @@ var rechnungenQuery = ctx.Verkaufsrechnungszeilens;
 var archive = ctx.NdeasyDokumentArchivs;
 var orderId = "324972342";
 
-var invoices = rechnungenQuery.AsQueryable()
-    .Where(q => q.AuftragsNummer == orderId)
-    .GroupBy(g => g.BelegNummer)
-    .Select(group => group.FirstOrDefault())
-    ;
 
-var i1=invoices
+var queryable = rechnungenQuery
+    .Where(q => q.AuftragsNummer == orderId)
+    .Select(x => x.BelegNummer)
+    .Distinct()
+    .Join(archive, o => o, archive => archive.BelegNummer, (Belegnr, archiv) => archiv)
+    .Where(w => w.Webdarstellung)
+    .OrderBy(x => x.BelegNummer)
+    .ThenBy(x => x.Dokumentenart)
+    .ThenBy(x => x.MandantenId)
+    .ThenByDescending(x => x.Version)
+    .GroupBy(x => new { x.BelegNummer, x.Dokumentenart, x.MandantenId })
+    .Select(g => new
+    {
+        g.Key.BelegNummer,
+        g.Key.Dokumentenart,
+        g.Key.MandantenId,
+        MaxVersion = g.Select(x => x.Version).Max()
+    });
+
+var invoices = queryable
+    .ToList();
+// .Select(s => new Document(s.BelegNummer, s.Dokumentenart, s.RootIdArchiv, s.MandantenId))
+// .ToList();
+
+var option2 = ctx.Database.SqlQuery<Document>($@"
+        SELECT DISTINCT [n].[BelegNummer], [n].[Dokumentenart], [n].[RootIdArchiv], [n].[MandantenId]
+        FROM [Verkaufsrechnungszeilen] [v] 
+        INNER JOIN [NdeasyDokumentArchiv] AS [n] ON [v].[BelegNummer] = [n].[BelegNummer]
+        JOIN (
+	        SELECT [n].[BelegNummer], [n].[Dokumentenart], [n].[MandantenId], MAX([n].[Version]) as MaxVersion
+	        FROM [NdeasyDokumentArchiv] [n]
+	        GROUP BY [n].[BelegNummer], [n].[Dokumentenart], [n].[MandantenId]
+        ) AS [n2]
+          ON n.BelegNummer = n2.BelegNummer
+            AND n.[Dokumentenart] = n2.[Dokumentenart]
+	        AND n.[MandantenId] = n2.[MandantenId]
+	        AND n.[Version] = n2.[MaxVersion]
+        WHERE [v].[AuftragsNummer] = {@orderId}
+          AND [n].[Webdarstellung] = CAST(1 AS bit)")
+        .ToList()
+
+// .GroupBy(g => g.BelegNummer)
+// .Select(group => group.First())
+;
+
+/*var i1=invoices
     .Where(s => s != null)
     .Select(s => s!.BelegNummer)
     .GroupJoin(archive, o => o, archive => archive.BelegNummer, (Belegnr, archiv) => archiv)
     .SelectMany(s => s);
 
 var i2 = i1.GroupBy(g => new { g.BelegNummer, g.Dokumentenart, g.MandantenId })
-    .Select(group => group.OrderByDescending(k => k.Version).FirstOrDefault())
-    .Where(s => s != null)
+    .Select(group => group.OrderByDescending(k => k.Version).First())
     ;
 
 var i3 = i2.Select(s => new Document(s!.BelegNummer, s.Dokumentenart, s.RootIdArchiv, s.MandantenId))
-    .ToList();
+    .ToList();*/
 
 Console.WriteLine("done");
 string x = "";
-    
-public sealed record Document(string BelegNummer, string DokumentenArt, string RootArchivId, string MandantenId);
+
+public sealed record Document(string BelegNummer, string DokumentenArt, string RootIdArchiv, string MandantenId);
